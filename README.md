890ec6a037cd92391286ee80609abff44d23b694
Rian Prakoso <mohammad.rian.ap@gmail.com>
2015-04-15 06:27:53 +0000

    - unique color msg for each user\n- add README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# Whiteboard
+
+This is the backend of collaborative whiteboard project that can be found [here](https://github.com/m-rap/whiteboard). This project is developed using node.js.
--- server.js
+++ server.js
@@ -33,21 +33,56 @@ console.log('starting server.js');
 
 var roomModel = require(__dirname + '/model.js').createRoomModel();
 var io = require('socket.io')(process.env.PORT);
+var clients = new Array();
+var colors = ['black', 'blue', 'fuchsia', 'green',
+              'maroon', 'navy', 'olive', 'orange', 'purple', 'red', 
+              'teal'];
+var curColor = 0;
 
 io.sockets.on('connection', function(socket) {
     console.log('new client ' + socket.id + ', ip: ' + socket.request.connection.remoteAddress);
     var lastLoadVer = null;
     var room = null;
+    var id = null;
     
     socket.on('disconnect', function() {
-        console.log('client ' + socket.id + ' has been disconnected');
+        console.log('client ' + socket.id + ((room != null) ? ' (' + room + ') ' : '') + ' has been disconnected');
+        if (room != null && id != null) {
+            clients[id] = null;
+            io.to(room).emit('client disconnected', id);
+        }
     });
     
     socket.on('start', function(data) {
+        var emptySlot = false;
+        var i = 0;
+        for (i in clients) {
+            if (clients[i] == null) {
+                emptySlot = true;
+                break;
+            }
+        }
+        var clientInfo = null;
+        if (emptySlot) {
+            clients[i] = {id: i, color: colors[curColor], room: data.room};
+            clientInfo = clients[i];
+        } else {
+            clients.push({id: clients.length, color: colors[curColor], room: data.room});
+            clientInfo = clients[clients.length - 1];
+        }
+        curColor = (curColor + 1) % colors.length;
+        
         socket.join(data.room);
         room = data.room;
         console.log('client ' + socket.id + ' has joined to ' + room + ' room');
-        socket.emit('start');
+        var clientsInRoom = new Array();
+        for (i in clients) {
+            if (i != clientInfo.id && clients[i].room == room) {
+                clientsInRoom.push(clients[i]);
+            }
+        }
+        socket.emit('start', {clientInfo: clientInfo, clients: clientsInRoom});
+        io.to(data.room).emit('new client', clientInfo);
     });
     socket.on('load', function(data) {
         if (data.version == lastLoadVer)
@@ -74,9 +109,13 @@ io.sockets.on('connection', function(socket) {
 		var version = roomModel.save(data.room, data.data);
 		data.data.version = version;
 		data.data.lastVersion = version;
-		socket.emit('submitSuccess');
 		io.to(data.room).emit('load', data.data);
 		//data = null;
 		//global.gc();
+	});
+	socket.on('chat message', function(m) {
+	    if (room == null)
+	        return;
+	    io.to(room).emit('chat message', m);
 	});
 });
